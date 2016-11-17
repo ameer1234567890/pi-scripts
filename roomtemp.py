aeaaa3c546a6ff2b7aaba1f2a703a005a1e36488
Ameer Dawod <ameer1234567890@gmail.com>
2016-11-18 00:16:28 +0500

    Change w1 to ds

--- roomtemp.py
+++ roomtemp.py
@@ -14,13 +14,13 @@ with open("/sys/bus/w1/devices/28-031663113dff/w1_slave", "r") as temp_file:
         line = re.findall(r't=.*', line)
         if line:
             line = line[0].split('=')[1]
-            w1_temp0 = int(line)
+            ds_temp0 = int(line)
 
-w1_temp1 = w1_temp0 / 1000
-w1_temp2 = w1_temp0 / 100
-w1_tempM = w1_temp2 % w1_temp1
-w1_temp = str(w1_temp1) + "." + str(w1_tempM)
-print("W1 Temperature: " + str(w1_temp + "°C"))
+ds_temp1 = ds_temp0 / 1000
+ds_temp2 = ds_temp0 / 100
+ds_tempM = ds_temp2 % ds_temp1
+ds_temp = str(ds_temp1) + "." + str(ds_tempM)
+print("DS Temperature: " + str(ds_temp + "°C"))
 
 
 dht_humidity, dht_temp = Adafruit_DHT.read_retry(dht_sensor, dht_pin)
@@ -33,6 +33,6 @@ else:
     print('Failed to get DHT reading. Try again!')
 
 
-maker_url = "https://maker.ifttt.com/trigger/roomtemp/with/key/" + maker_key + "?value1=" + w1_temp + "&value2=" + str(dht_temp) + "&value3=" + str(dht_humidity)
+maker_url = "https://maker.ifttt.com/trigger/roomtemp/with/key/" + maker_key + "?value1=" + ds_temp + "&value2=" + str(dht_temp) + "&value3=" + str(dht_humidity)
 content = urllib2.urlopen(maker_url).read()
 print(content)
