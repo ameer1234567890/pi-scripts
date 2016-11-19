43c20b32ed9e0d5a3ca3223feee0d7521d1ab4ef
Ameer Dawod <ameer1234567890@gmail.com>
2016-11-19 11:05:38 +0500

    Verbose output in speedometer

--- speedometer.py
+++ speedometer.py
@@ -14,11 +14,13 @@ NUM_LEDS = len(MCP_PINS)
 
 def get_speed():
     global speed
+    global raw_speed
     with open(LOG_FILE, 'r') as log_file:
         for line in log_file:
             line = re.findall(r'Download.*', line)
             if line:
                 line = line[0].split(': ')[1].split(' Mbit/s')[0]
+                raw_speed = line
                 speed = int(math.ceil(float(line)))
                 if speed > SPEED_LIMIT: speed = SPEED_LIMIT
 
@@ -34,11 +36,11 @@ if os.stat(LOG_FILE).st_size == 0:
 else:
     get_speed()
 
-print('Speed: ' + str(speed) + ' Mbits/s')
+print('Speed: ' + str(raw_speed) + ' Mbits/s')
 
 speed_percent = (speed * 100) / SPEED_LIMIT
 leds_lit = (NUM_LEDS * speed_percent) / 100
-print('LEDs lit: ' + str(leds_lit))
+print('LEDs lit: ' + str(leds_lit) + '/' + str(NUM_LEDS))
 
 wiringpi.wiringPiSetup()
 wiringpi.mcp23017Setup(PIN_BASE,I2C_ADDR)
