ea2e63c6d1ae14487b9247673a03142be35432b3
Ameer Dawod <ameer1234567890@gmail.com>
2016-12-06 15:40:24 +0500

    Some fixes in door.py

--- door.py
+++ door.py
@@ -3,6 +3,7 @@ import os
 import time
 import datetime
 import multiprocessing
+import requests
 
 SENSOR_PIN = 14
 LED_PIN = 15
@@ -21,25 +22,31 @@ with open("/home/pi/.maker_key", "r") as key_file:
 with open(PID_FILE, 'w') as fh:
     fh.write(str(os.getpid()))
 
-def check_door():
-    if os.path.isfile(STATE_FILE) == False:
-        with open(STATE_FILE, 'w') as fh:
-            fh.write('1')
+if os.path.isfile(STATE_FILE) == False:
+    with open(STATE_FILE, 'w') as fh:
+        fh.write('1')
         GPIO.output(LED_PIN, 1)
+
+with open(STATE_FILE, 'r') as fh:
+    state = fh.read()
+
+if state == '1':
+    GPIO.output(LED_PIN, 1)
+else:
+    GPIO.output(LED_PIN, 0)
+
+def check_door():
     try:
         while 1:
-            with open(STATE_FILE, 'r') as fh:
-                state = fh.read()
             if state == '1':
-                GPIO.outpur(LED_PIN, 1)
-                GPIO.wait_for_edge(SENSOR_PIN, GPIO.FALLING)
+                GPIO.wait_for_edge(SENSOR_PIN, GPIO.RISING)
                 print('Door movement at %s' % datetime.datetime.now())
                 maker_url = 'https://maker.ifttt.com/trigger/door/with/key/' + maker_key
                 content = requests.get(maker_url).text
                 print(content)
                 time.sleep(1)
             elif state == '0':
-                GPIO.output(LED_PIN, 0)
+                print('Not armed')
                 time.sleep(READ_STATE_INTERVAL)
             else:
                 print('Error: State file in unknown state!')
@@ -51,11 +58,13 @@ def arm_sensor():
     with open(STATE_FILE, 'w') as fh:
         fh.write('1')
     GPIO.output(LED_PIN, 1)
+    state = 1
 
 def disarm_sensor():
     with open(STATE_FILE, 'w') as fh:
         fh.write('0')
     GPIO.output(LED_PIN, 0)
+    state = 0
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 class S(BaseHTTPRequestHandler):
