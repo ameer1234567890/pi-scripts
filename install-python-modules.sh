a3f24c7eae95b211faf776a71bdc3c620324eb51
Ameer Dawod <ameer1234567890@gmail.com>
2016-12-11 10:55:18 +0500

    Add additional modules

--- install-python-modules.sh
+++ install-python-modules.sh
@@ -2,3 +2,14 @@ modules=(RPi.GPIO httplib2 google-api-python-client oauth2client Adafruit_DHT wi
 for module in ${modules[@]}; do
     sudo pip install $module
 done
+
+#vcgencmd
+sudo pip install git+https://github.com/nicmcd/vcgencmd.git
+
+#Adafruit-DHT
+sudo apt-get install build-essential python-dev
+git clone https://github.com/adafruit/Adafruit_Python_DHT
+cd Adafruit_Python_DHT
+sudo python setup.py install
+cd ..
+rm -rf Adafruit_Python_DHT
