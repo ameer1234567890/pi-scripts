5929d27aeb084edaf07ce93561f022e1cd987551
Ameer Dawood <ameer1234567890@gmail.com>
2017-01-18 13:20:48 +0500

    Combine WUstation with roomtemp

--- roomtemp.py
+++ roomtemp.py
@@ -2,9 +2,25 @@
 import re
 import requests
 import Adafruit_DHT
+import math
+execfile('/home/pi/.wu_config.py')
+from urllib import urlencode
+import urllib2
 
-dht_sensor = Adafruit_DHT.DHT11
-dht_pin = 18
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 18
+WU_URL = "http://weatherstation.wunderground.com/weatherstation/updateweatherstation.php"
+
+def c_to_f(input_temp):
+    # convert input_temp from Celsius to Fahrenheit
+    return (input_temp * 1.8) + 32
+
+def dew_point(celsius, humidity):
+    a = 17.271
+    b = 237.7
+    temp = (a * celsius) / (b + celsius) + math.log(humidity*0.01)
+    dew = (b * temp) / (a - temp)
+    return dew
 
 with open('/home/pi/.maker_key', 'r') as key_file:
     maker_key = key_file.read()
@@ -22,17 +38,36 @@ ds_tempM = ds_temp2 % ds_temp1
 ds_temp = str(ds_temp1) + '.' + str(ds_tempM)
 print('DS Temperature: %s°C' % str(ds_temp))
 
-
-dht_humidity, dht_temp = Adafruit_DHT.read_retry(dht_sensor, dht_pin)
+dht_humidity, dht_temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
 if dht_humidity is not None and dht_temp is not None:
     print('DHT Temperature: {0:0.1f}°C DHT Humidity: {1:0.1f}%'.format(dht_temp, dht_humidity))
+    dewpoint = dew_point(int(float(ds_temp)), dht_humidity)
+    print('Dew Point: %s°C' % str(round(dewpoint, 2)))
+
+	# Post to Weather Underground PWS (Personal Weather Station)
+    print('Uploading data to Weather Underground...')
+    weather_data = {
+        'action': 'updateraw',
+        'ID': Config.STATION_ID,
+        'PASSWORD': Config.STATION_KEY,
+        'dateutc': 'now',
+        'tempf': str(c_to_f(int(float(ds_temp)))),
+        'humidity': str(dht_humidity),
+        'dewptf': str(c_to_f(dewpoint)),
+    }
+    upload_url = WU_URL + '?' + urlencode(weather_data)
+    response = urllib2.urlopen(upload_url)
+    html = response.read()
+    print('Server response: ' + html)
+    response.close()
+
+	# Post to Google Spreadsheet via IFTTT
+    print('Triggerring IFTTT event...')
+    maker_url = 'https://maker.ifttt.com/trigger/roomtemp/with/key/' + maker_key + '?value1=' + ds_temp + '&value2=' + str(dht_humidity) + '&value3=' + str(round(dewpoint, 2))
+    content = requests.get(maker_url).text
+    print(content)
 else:
     dht_temp = 0
     dht_humidity = 0
     print('Failed to get DHT reading. Try again!')
-
-
-maker_url = 'https://maker.ifttt.com/trigger/roomtemp/with/key/' + maker_key + '?value1=' + ds_temp + '&value2=' + str(dht_temp) + '&value3=' + str(dht_humidity)
-content = requests.get(maker_url).text
-print(content)
