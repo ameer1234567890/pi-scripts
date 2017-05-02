77b251771dbf63ccee8a11ceaf14c8ee5bbda36a
Ameer Dawood <ameer1234567890@gmail.com>
2017-05-02 22:28:31 +0500

    Update install-python-modules.sh

--- install-python-modules.sh
+++ install-python-modules.sh
@@ -1,4 +1,4 @@
-modules=(RPi.GPIO httplib2 google-api-python-client oauth2client wiringpi requests)
+modules=(RPi.GPIO httplib2 google-api-python-client oauth2client wiringpi requests Pillow psutil)
 for module in ${modules[@]}; do
     sudo pip install $module
 done
@@ -13,3 +13,11 @@ cd Adafruit_Python_DHT
 sudo python setup.py install
 cd ..
 sudo rm -rf Adafruit_Python_DHT
+
+#Adafruit-SSD1306
+sudo apt-get install build-essential python-dev
+git clone https://github.com/adafruit/Adafruit_Python_SSD1306
+cd Adafruit_Python_SSD1306
+sudo python setup.py install
+cd ..
+sudo rm -rf Adafruit_Python_SSD1306
