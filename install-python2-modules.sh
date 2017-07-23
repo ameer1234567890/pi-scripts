b1ccf6d6b451dd5c410dcc43c6578369160df397
Ameer Dawood <ameer1234567890@gmail.com>
2017-07-23 16:44:30 +0500

    Add separate install files for python2 and python3

---
+++ install-python2-modules.sh
@@ -0,0 +1,31 @@
+modules=(RPi.GPIO httplib2 google-api-python-client oauth2client requests Pillow psutil)
+for module in ${modules[@]}; do
+    sudo pip2 install $module
+done
+
+#vcgencmd
+sudo pip2 install git+https://github.com/nicmcd/vcgencmd.git
+
+# Install build tools
+sudo apt-get install build-essential python-dev
+
+#Adafruit-DHT
+git clone https://github.com/adafruit/Adafruit_Python_DHT
+cd Adafruit_Python_DHT
+sudo python2 setup.py install
+cd ..
+sudo rm -rf Adafruit_Python_DHT
+
+#Adafruit-GPIO
+git clone https://github.com/adafruit/Adafruit_Python_GPIO
+cd Adafruit_Python_GPIO
+sudo python2 setup.py install
+cd ..
+sudo rm -rf Adafruit_Python_GPIO
+
+#Adafruit-SSD1306
+git clone https://github.com/adafruit/Adafruit_Python_SSD1306
+cd Adafruit_Python_SSD1306
+sudo python2 setup.py install
+cd ..
+sudo rm -rf Adafruit_Python_SSD1306
--- install-python3-modules.sh
+++ install-python3-modules.sh
@@ -1,10 +1,10 @@
 modules=(RPi.GPIO httplib2 google-api-python-client oauth2client requests Pillow psutil)
 for module in ${modules[@]}; do
-    sudo pip install $module
+    sudo pip3 install $module
 done
 
 #vcgencmd
-sudo pip install git+https://github.com/nicmcd/vcgencmd.git
+sudo pip3 install git+https://github.com/nicmcd/vcgencmd.git
 
 # Install build tools
 sudo apt-get install build-essential python-dev
@@ -12,20 +12,20 @@ sudo apt-get install build-essential python-dev
 #Adafruit-DHT
 git clone https://github.com/adafruit/Adafruit_Python_DHT
 cd Adafruit_Python_DHT
-sudo python setup.py install
+sudo python3 setup.py install
 cd ..
 sudo rm -rf Adafruit_Python_DHT
 
 #Adafruit-GPIO
 git clone https://github.com/adafruit/Adafruit_Python_GPIO
 cd Adafruit_Python_GPIO
-sudo python setup.py install
+sudo python3 setup.py install
 cd ..
 sudo rm -rf Adafruit_Python_GPIO
 
 #Adafruit-SSD1306
 git clone https://github.com/adafruit/Adafruit_Python_SSD1306
 cd Adafruit_Python_SSD1306
-sudo python setup.py install
+sudo python3 setup.py install
 cd ..
 sudo rm -rf Adafruit_Python_SSD1306
