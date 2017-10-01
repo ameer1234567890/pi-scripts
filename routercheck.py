bc3cef94ee51661114e39b928c085d2844ff234d
Ameer Dawood <ameer1234567890@gmail.com>
2017-10-01 13:15:56 +0500

    Add log file for reboot events

--- routercheck.py
+++ routercheck.py
@@ -2,9 +2,11 @@ from __future__ import print_function
 import RPi.GPIO as GPIO
 import os
 import time
+import datetime
 
 PIN = 17
 ROUTER_IP = '192.168.7.1'
+LOG_FILE = '/home/pi/pi-scripts/routercheck.log'
 
 print('Checking connectivity...')
 response = os.system('ping -c 1 ' + ROUTER_IP + ' >> /dev/null 2>&1')
@@ -23,6 +25,8 @@ else:
         print('Router was OK! Wifi was down! Now OK!')
     else:
         print('Router is actually down! Rebooting router now...')
+        with open(LOG_FILE, 'a') as fh:
+          fh.write('{} Rebooting router!\n'.format(datetime.datetime.now()))
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(PIN, GPIO.OUT)
         GPIO.output(PIN, 1)
