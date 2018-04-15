08c2340e7938999e0e441b5912c0f641421d2c40
Ameer Dawood <ameer1234567890@gmail.com>
2018-04-15 18:45:22 +0500

    Add looping to routercheck

--- routercheck.py
+++ routercheck.py
@@ -7,29 +7,31 @@ import datetime
 PIN = 17
 ROUTER_IP = '192.168.7.1'
 LOG_FILE = '/home/pi/pi-scripts/routercheck.log'
+INTERVAL = 60 # in seconds
 
-print('{} Checking connectivity...'.format(datetime.datetime.now()))
-response = os.system('ping -c 1 ' + ROUTER_IP + ' >> /dev/null 2>&1')
-
-if response == 0:
-    print('Router is OK!')
-else:
-    print('{} Either router is not working or Wifi is down. Rebooting Wifi...'.format(datetime.datetime.now()))
-    wlan_if = os.popen('ifconfig | grep wl | cut -d : -f 1').read()
-    os.system('sudo ifdown ' + wlan_if)
-    os.system('sudo ifup ' + wlan_if)
-    time.sleep(1)
+while True:
     print('{} Checking connectivity...'.format(datetime.datetime.now()))
-    response = os.system('ping -c 1 ' + ROUTER_IP + '>> /dev/null 2>&1')
+    response = os.system('ping -c 1 ' + ROUTER_IP + ' >> /dev/null 2>&1')
     if response == 0:
-        print('{} Router was OK! Wifi was down! Now OK!'.format(datetime.datetime.now()))
+        print('Router is OK!')
     else:
-        print('{} Router is actually down! Rebooting router now...'.format(datetime.datetime.now()))
-        with open(LOG_FILE, 'a') as fh:
-          fh.write('{} Rebooting router!\n'.format(datetime.datetime.now()))
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(PIN, GPIO.OUT)
-        GPIO.output(PIN, 1)
+        print('{} Either router is not working or Wifi is down. Rebooting Wifi...'.format(datetime.datetime.now()))
+        wlan_if = os.popen('ifconfig | grep wl | cut -d : -f 1').read()
+        os.system('sudo ifdown ' + wlan_if)
+        os.system('sudo ifup ' + wlan_if)
         time.sleep(1)
-        GPIO.output(PIN, 0)
-        GPIO.cleanup()
+        print('{} Checking connectivity...'.format(datetime.datetime.now()))
+        response = os.system('ping -c 1 ' + ROUTER_IP + '>> /dev/null 2>&1')
+        if response == 0:
+            print('{} Router was OK! Wifi was down! Now OK!'.format(datetime.datetime.now()))
+        else:
+            print('{} Router is actually down! Rebooting router now...'.format(datetime.datetime.now()))
+            with open(LOG_FILE, 'a') as fh:
+                fh.write('{} Rebooting router!\n'.format(datetime.datetime.now()))
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(PIN, GPIO.OUT)
+            GPIO.output(PIN, 1)
+            time.sleep(1)
+            GPIO.output(PIN, 0)
+            GPIO.cleanup()
+    time.sleep(INTERVAL)
