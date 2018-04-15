c6ca9117461cdd9ab9c1306371a908967f53e9e1
Ameer Dawood <ameer1234567890@gmail.com>
2018-04-15 23:58:28 +0500

    Add service uninstaller

---
+++ uninstall-service.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -o errexit
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root (use sudo)" 1>&2
+  exit 1
+fi
+
+if [ "$1" == "" -o "$1" == "-h" -o "$1" == "--help" ]; then
+  echo "Usage: sudo ${0} filename.py"
+  exit 0
+fi
+
+file="$1"
+
+if [ ! -f "$file" ]; then
+  echo "Error: $file not found!"
+  exit 1
+fi
+
+service_name="$(echo $file | cut -d '.' -f 1)"
+service_name="${service_name}.service"
+
+echo "Stopping ${service_name}..."
+sudo systemctl stop ${service_name}
+echo "Disabling ${service_name}..."
+sudo systemctl disable ${service_name}
+echo "Removing ${service_name}..."
+sudo rm /etc/systemd/system/${service_name}
+echo "========================="
