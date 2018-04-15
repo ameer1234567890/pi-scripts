2d2757d86a895031f4578e72cf9a499956acbeee
Ameer Dawood <ameer1234567890@gmail.com>
2018-04-15 23:38:21 +0500

    Add loop to pitemp

--- pitemp.py
+++ pitemp.py
@@ -5,36 +5,42 @@ import requests
 import os
 import time
 
-temp_limit = 50
-notify_interval = 30
-log_file_path = '/home/pi/pi-scripts/pitemp.log'
+TEMP_LIMIT = 50
+NOTIFY_INTERVAL = 30
+INTERVAL = 60 # in seconds
+LOG_FILE = '/home/pi/pi-scripts/pitemp.log'
 
 with open('/home/pi/.maker_key', 'r') as key_file:
     maker_key = key_file.read()
 
-cpu_temp = vcgencmd.measure_temp()
+while True:
 
-if os.path.isfile(log_file_path) == False:
-    with open(log_file_path, 'w') as log_file:
-        log_file.write('%s' % time.time())
+    cpu_temp = vcgencmd.measure_temp()
 
-with open(log_file_path, 'r') as log_file:
-    time_last_notif = float(log_file.read())
+    if os.path.isfile(LOG_FILE) == False:
+        with open(LOG_FILE, 'w') as fh:
+            fh.write('%s' % time.time())
 
-maker_url = 'https://maker.ifttt.com/trigger/pitemp/with/key/' + maker_key + '?value1=' + str(cpu_temp)
-content = requests.get(maker_url).text
-print(content)
+    with open(LOG_FILE, 'r') as fh:
+        time_last_notif = float(fh.read())
 
-if cpu_temp > temp_limit:
-    time_now = time.time()
-    if time_now > time_last_notif + notify_interval:
-        print('High and notify - {}°C'.format(str(cpu_temp)))
-        maker_url = 'https://maker.ifttt.com/trigger/pitemp-over/with/key/' + maker_key + '?value1=' + str(cpu_temp)
-        content = requests.get(maker_url).text
-        print(content)
-        with open(log_file_path, 'w') as log_file:
-            log_file.write(time.time())
+    maker_url = 'https://maker.ifttt.com/trigger/pitemp/with/key/' + maker_key + '?value1=' + str(cpu_temp)
+    content = requests.get(maker_url).text
+    print(content)
+
+    if cpu_temp > TEMP_LIMIT:
+        time_now = time.time()
+        if time_now > time_last_notif + NOTIFY_INTERVAL:
+            print('High and notify - {}°C'.format(str(cpu_temp)))
+            maker_url = 'https://maker.ifttt.com/trigger/pitemp-over/with/key/' + maker_key + '?value1=' + str(cpu_temp)
+            content = requests.get(maker_url).text
+            print(content)
+            with open(LOG_FILE, 'w') as fh:
+                fh.write('%s' % time.time())
+        else:
+            print('High and not notify - {}°C'.format(str(cpu_temp)))
     else:
-        print('High and not notify - {}°C'.format(str(cpu_temp)))
-else:
-    print('Not high - {}°C'.format(str(cpu_temp)))
+        print('Not high - {}°C'.format(str(cpu_temp)))
+
+    print('======= DONE =======')
+    time.sleep(INTERVAL)
