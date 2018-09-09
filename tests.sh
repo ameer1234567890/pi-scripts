2097cdb0ac459db936616fdda76470d7504d669b
Ameer Dawood <ameer1234567890@gmail.com>
2018-09-09 13:47:38 +0500

    Add tool checking

--- tests.sh
+++ tests.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+check_tools(){
+  tools="flake8 shellcheck"
+  for tool in $tools; do
+    if [ ! "$(command -v "$tool")" ]; then
+      printf "\e[1m%s\e[0m not found! Exiting....\n" "$tool"
+      exit 1
+    fi
+  done
+}
+
+check_tools
+
 has_errored=false
 find . ! -name "$(printf "*\n*")" -name '*.py' | grep -v './env' > tmp
 while IFS= read -r file; do
