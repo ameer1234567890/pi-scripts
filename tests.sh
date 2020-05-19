66bbbbaa470ae1e2cf6b4cacb8109c82358357ec
Ameer Dawood <ameer1234567890@gmail.com>
2020-05-19 12:46:44 +0500

    Ignore camelCase in RPi.GPIO

--- tests.sh
+++ tests.sh
@@ -16,7 +16,7 @@ has_errored=false
 find . ! -name "$(printf "*\n*")" -name '*.py' | grep -v './env' | grep -v './python-broadlink' > tmp
 while IFS= read -r file; do
   printf "Running flake8 on %s.... Result: " "$file"
-  flake8 --count "$file"
+  flake8 --count --ignore=N814 "$file"
   flake8_status="$?"
   if [ "$flake8_status" != 0 ]; then
     has_errored=true
